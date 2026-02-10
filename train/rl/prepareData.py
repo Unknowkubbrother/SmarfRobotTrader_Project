5a69f77e766ea5adf1bda489e121a27573151d4b
Unknowkubbrother <natchnon654321@gmail.com>
2026-02-10 17:14:32 +0700

    update

--- train/rl/prepareData.py
+++ train/rl/prepareData.py
@@ -35,7 +35,7 @@ TIMEFRAME_CONFIG = {
 SELECTED_TIMEFRAME = "H1"  # Change this to "M5", "M15", etc.
 tf_config = TIMEFRAME_CONFIG[SELECTED_TIMEFRAME]
 
-ohlc_start = datetime(2026, 2, 1, tzinfo=timezone)
+ohlc_start = datetime(2026, 1, 1, tzinfo=timezone)
 ohlc_end   = datetime(2026, 2, 8, 23, 59, 59, tzinfo=timezone)
 
 
